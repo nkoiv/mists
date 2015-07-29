eaf650071f6b51c9f024d150a850d2b09745f6f4
nkoiv <niko.koivumaki@gmail.com>
2015-07-29 20:56:44 +0300

    Added enumerator for directions

---
+++ mists-game/src/main/java/com/nkoiv/mists/game/Direction.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nkoiv.mists.game;
+
+/**
+ *
+ * @author nkoiv
+ */
+
+public enum Direction {
+	UP, DOWN, LEFT, RIGHT, UPLEFT, UPRIGHT, DOWNLEFT, DOWNRIGHT, STAY,
+
+}
+
+
