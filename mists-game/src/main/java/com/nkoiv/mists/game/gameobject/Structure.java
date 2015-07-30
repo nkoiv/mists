88e356db4966a0602564ca6de8f05983992119ca
nkoiv <niko.koivumaki@gmail.com>
2015-07-30 19:46:19 +0300

    Added rendering for extra sprites

--- mists-game/src/main/java/com/nkoiv/mists/game/gameobject/Structure.java
+++ mists-game/src/main/java/com/nkoiv/mists/game/gameobject/Structure.java
@@ -5,23 +5,70 @@
  */
 package com.nkoiv.mists.game.gameobject;
 
+import com.nkoiv.mists.game.sprites.Sprite;
 import com.nkoiv.mists.game.world.Location;
+import java.util.ArrayList;
+import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
 
 /**
- *
- * @author lp35567
+ * Structures are MapObjects with varying collision boxes
+ * A single structure may have some parts of it causing collision, others not
+ * @author nkoiv
  */
 public class Structure extends MapObject {
+    
+    private ArrayList<Sprite> extraSprites;
 
     public Structure(String name, Image image, int collisionLevel) {
         super(name, image);
         super.setCollisionLevel(collisionLevel);
+        this.extraSprites = new ArrayList<>();
     }
     
     public Structure(String name, Image image, Location location, int xCoor, int yCoor) {
         super(name, image, location, xCoor, yCoor);
         this.setCollisionLevel(100);
+        this.extraSprites = new ArrayList<>();
+    }
+    
+    public void addExtra (Sprite sprite, double xOffset, double yOffset) {
+        sprite.setPosition(this.getxPos()+xOffset, this.getyPos()+yOffset);
+        this.extraSprites.add(sprite);
+    }
+    
+    public void addExtra (Image image, double xOffset, double yOffset) {
+        this.extraSprites.add(new Sprite(
+                image, this.getxPos()+xOffset, this.getyPos()+yOffset ));
+    }
+    
+    public ArrayList<Sprite> getExtras() {
+        return this.extraSprites;
+    }
+    
+    public void removeExtras() {
+        this.extraSprites.clear();
+    }
+
+    public void renderExtras (double xOffset, double yOffset, GraphicsContext gc) {
+        if (!this.extraSprites.isEmpty()) {
+            for (Sprite extraSprite : this.extraSprites) {
+                extraSprite.render(xOffset, yOffset, gc);
+            }
+        }
+    }
+    
+    //setPosition is overwritten to move extras along with the main sprite
+    @Override
+    public void setPosition (double xPos, double yPos) {
+        if (!this.extraSprites.isEmpty()) {
+            for (Sprite extraSprite : extraSprites) { //All extras are moved keeping the same relation to the main sprite
+                double xOffset = this.getSprite().getXPos() - extraSprite.getXPos();
+                double yOffset = this.getSprite().getYPos() - extraSprite.getYPos();
+                extraSprite.setPosition(xPos-xOffset, yPos-yOffset);
+            }
+        }
+        this.getSprite().setPosition(xPos, yPos);
     }
     
 }
