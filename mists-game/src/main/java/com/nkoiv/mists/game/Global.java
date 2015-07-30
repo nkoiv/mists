d7a466ceb872efdb435ab253a9cf778d0b9275cc
nkoiv <niko.koivumaki@gmail.com>
2015-07-30 19:45:49 +0300

    Updated global variables

---
+++ mists-game/src/main/java/com/nkoiv/mists/game/Global.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nkoiv.mists.game;
+
+/**
+ * The Global interface works as a dummy global config -file
+ * @author nkoiv
+ */
+public interface Global {
+    
+    //TileSize
+    public static final int SCALE = 1;
+    public static final int TILESIZE = 32;
+    public static final int WIDTH = 800;
+    public static final int HEIGHT = 600;
+    public static final int TILES_DRAWN_X = (WIDTH/TILESIZE)+1;
+    public static final int TILES_DRAWN_Y = (HEIGHT/TILESIZE)+1;
+    public static final float MAXIMUM_STEP = 0.5f;
+	
+    //Config mode
+    public static final boolean debug = true;
+    
+}
