c08787f3c18e8d5aa2a2bf03d8573ce24db439bf
Niko Koivumäki <niko.koivumaki@gmail.com>
2015-07-30 11:35:24 +0300

    Added collision detection

--- mists-game/src/main/java/com/nkoiv/mists/game/world/Location.java
+++ mists-game/src/main/java/com/nkoiv/mists/game/world/Location.java
@@ -8,7 +8,10 @@ package com.nkoiv.mists.game.world;
 import com.nkoiv.mists.game.Global;
 import com.nkoiv.mists.game.gameobject.MapObject;
 import com.nkoiv.mists.game.gameobject.PlayerCharacter;
+import com.nkoiv.mists.game.gameobject.Structure;
+import com.nkoiv.mists.game.sprites.Sprite;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.image.Image;
@@ -23,7 +26,7 @@ public class Location implements Global {
     * TODO: Lists for various types of MapObjects, from creatures to frills.
     */
     private final List<MapObject> mapObjects;
-    
+    private String name;
     private Map map;
     
     private MapObject screenFocus;
@@ -33,17 +36,25 @@ public class Location implements Global {
     * Constructer for demofield
     */
     public Location() {
+        this.name = "POCmap";
         this.mapObjects = new ArrayList<>();
         this.map = new BGMap(new Image("/images/pocmap.png"));
         
-        PlayerCharacter himmu;
-        himmu = new PlayerCharacter("Himmu", new Image("/images/himmu.png"));
+        PlayerCharacter himmu = new PlayerCharacter("Himmu", new Image("/images/himmu.png"));
         himmu.getSprite().setPosition(300, 200);
+        himmu.setLocation(this);
         this.setPlayer(himmu);
         this.mapObjects.add(himmu);
         this.screenFocus = himmu;
         
-  
+        Structure block = new Structure("CollisionTest", new Image("/images/block.png"), this, 450, 350);
+        this.mapObjects.add(block);
+    }
+    
+    public void addStructure(String name, Image image, int xPos, int yPos) {
+        MapObject newMob = new MapObject(name, image);
+        newMob.setLocation(this);
+        newMob.getSprite().setPosition(xPos, yPos);
     }
     
     public void setMap(Map m) {
@@ -64,12 +75,12 @@ public class Location implements Global {
     
     public double getxOffset(double xPos){
 	//Calculate Offset to ensure Player is centered on the screen
-        double xOffset = xPos - (TILES_DRAWN_X*TILESIZE) / 2;
+        double xOffset = xPos - (WIDTH) / 2;
         //Prevent leaving the screen
         if (xOffset < 0) {
             xOffset = 0;
-        } else if (xOffset > map.getWidth() -(TILES_DRAWN_X*TILESIZE)) {
-            xOffset = map.getWidth() - (TILES_DRAWN_X*TILESIZE);
+        } else if (xOffset > map.getWidth() -(WIDTH)) {
+            xOffset = map.getWidth() - (WIDTH);
         }
         
         return xOffset;
@@ -77,27 +88,52 @@ public class Location implements Global {
 	
     public double getyOffset(double yPos){
 	//Calculate Offset to ensure Player is centered on the screen
-        double yOffset = yPos - (TILES_DRAWN_Y*TILESIZE) / 2;
+        double yOffset = yPos - (HEIGHT) / 2;
         //Prevent leaving the screen
         if (yOffset < 0) {
             yOffset = 0;
-        } else if (yOffset > map.getHeight() -(TILES_DRAWN_Y*TILESIZE)) {
-            yOffset = map.getHeight() - (TILES_DRAWN_Y*TILESIZE);
+        } else if (yOffset > map.getHeight() -(HEIGHT)) {
+            yOffset = map.getHeight() - (HEIGHT);
         }
         
         return yOffset;
 	}
     
     public void update (double time) {
+        /*
+        * Update is the main "tick" of the Location.
+        * Movement, combat and triggers should all be handled here
+        */
         if (!this.mapObjects.isEmpty()) {
             for (MapObject mob : this.mapObjects) {
-                mob.getSprite().update(time);
+                mob.update(time);
             }    
         }
     }
     
+    public MapObject checkCollisions (Sprite s) {
+        /*
+        * TODO: Maybe only check collisions from nearby objects?
+        */
+        Iterator<MapObject> mapObjectsIter = mapObjects.iterator();
+        while ( mapObjectsIter.hasNext() )
+        {
+            MapObject collidingObject = mapObjectsIter.next();
+            if (!collidingObject.getSprite().equals(s)) { // Colliding with yourself is not really a collision
+                if ( s.intersects(collidingObject.getSprite()) ) 
+                 {
+                    return collidingObject;
+                }
+            }
+        }
+        return null;
+    }
+    
     public void render (GraphicsContext gc) {
         
+        /*
+        * Update Offsets first to know which parts of the location are drawn
+        */
         double xOffset = getxOffset(screenFocus.getSprite().getXPos());
         double yOffset = getyOffset(screenFocus.getSprite().getYPos());
         
@@ -112,6 +148,17 @@ public class Location implements Global {
         }
         
         
+    }
+    
+    public String getName() {
+        return this.name;
+    }
+    
+    @Override
+    public String toString() {
+        String s;
+        s = this.name+", a "+this.map.getWidth()+"x"+this.map.getHeight()+" area with "+this.mapObjects.size()+" mobs";
+        return s;
     }
     
 }
