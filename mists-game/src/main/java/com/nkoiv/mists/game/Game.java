0bbfad3b4ee659569722eb54cb224ffeb4805e6a
Niko Koivumäki <niko.koivumaki@gmail.com>
2015-07-31 11:13:35 +0300

    Separated logical engine into Game.java

---
+++ mists-game/src/main/java/com/nkoiv/mists/game/Game.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nkoiv.mists.game;
+
+import static com.nkoiv.mists.game.Mists.logger;
+import com.nkoiv.mists.game.world.Location;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+
+/**
+ *
+ * @author lp35567
+ */
+public class Game {
+    
+    public Location currentLocation;
+    public boolean running = false;
+    public final ArrayList<String> inputLog = new ArrayList<>();
+    public Scene currentScene;
+    public double xOffset; //Offsets are used control which part of the map is drawn
+    public double yOffset; //If/when a map is larger than display-area, it should be centered on player
+    
+    boolean inMenu = false;
+    
+    public Game () {
+        currentLocation = new Location();
+    }
+    
+    public void moveToLocation (Location l) {
+        currentLocation = l;
+    }
+ 
+    public void tick(double time, ArrayList<String> inputLog) {
+        /*
+        * Tick checks keybuffer, initiates actions and does just about everything.
+        * Tick needs to know how much time has passed since the last tick, so it can
+        * even out actions and avoid rollercoaster game speed. 
+        */
+        handleKeyPress(inputLog);
+        currentLocation.update(time);
+
+    }
+    
+    public void render(Canvas canvas) {
+        /*
+        * Render handles updating the game window, and should be called every time something needs refreshed.
+        * By default render is called 60 times per second (or as close to as possible) by AnimationTimer -thread.
+        */
+        GraphicsContext gc = canvas.getGraphicsContext2D();
+        gc.clearRect(0, 0, 800, 600);
+        currentLocation.render(gc);
+        //logger.info("Rendered on canvas");
+    }
+    
+    private void handleKeyPress(ArrayList<String> inputLog) {
+        /*
+        * TODO: External loadable configfile for keybindings
+        * (probably via a class of its own)
+        */
+        
+        currentLocation.getPlayer().stopMovement();
+        
+        if (inputLog.isEmpty()) {
+            return;
+        }
+
+        
+        if (inputLog.contains("UP")) {
+            Mists.logger.log(Level.INFO, "Moving {0} UP", currentLocation.getPlayer().getName());
+            currentLocation.getPlayer().moveTowards(Direction.UP);            
+        }
+        if (inputLog.contains("DOWN")) {
+            Mists.logger.log(Level.INFO, "Moving {0} DOWN", currentLocation.getPlayer().getName());
+            currentLocation.getPlayer().moveTowards(Direction.DOWN);
+        }
+        if (inputLog.contains("LEFT")) {
+            Mists.logger.log(Level.INFO, "Moving {0} LEFT", currentLocation.getPlayer().getName());
+            currentLocation.getPlayer().moveTowards(Direction.LEFT);
+        }
+        if (inputLog.contains("RIGHT")) {
+            Mists.logger.log(Level.INFO, "Moving {0} RIGHT", currentLocation.getPlayer().getName());
+            currentLocation.getPlayer().moveTowards(Direction.RIGHT);
+        }
+        
+        
+        
+    }
+}
--- mists-game/src/main/java/com/nkoiv/mists/game/Mists.java
+++ mists-game/src/main/java/com/nkoiv/mists/game/Mists.java
@@ -7,7 +7,6 @@ package com.nkoiv.mists.game;
 
 
 import static com.nkoiv.mists.game.Mists.logger;
-import com.nkoiv.mists.game.world.Location;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -17,35 +16,24 @@ import javafx.event.EventHandler;
 import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.canvas.Canvas;
-import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.input.KeyEvent;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
-import javafx.stage.Screen;
 import javafx.stage.Stage;
 
 /**
- *
- * TODO: Separate a main Game-class from this one
- * something that can be instanced easily
+ * Mists Class is used to initialize the window and launch the game
+ * This class is intended to be easily rewritten when porting game to other platforms
  * @author nkoiv
  */
 public class Mists extends Application implements Global {
      
     public static final Logger logger = Logger.getLogger(Mists.class.getName());
     
-    public Location currentLocation;
+    public Game MistsGame;
+    
     public boolean running = false;
     public final ArrayList<String> inputLog = new ArrayList<>();
     public Scene currentScene;
-    public double xOffset; //Offsets are used control which part of the map is drawn
-    public double yOffset; //If/when a map is larger than display-area, it should be centered on player
     
-    boolean inMenu = false;
-    
-    public void moveToLocation (Location l) {
-        currentLocation = l;
-    }
 
     @Override
     public void start(Stage primaryStage) {
@@ -61,7 +49,7 @@ public class Mists extends Application implements Global {
         primaryStage.setScene(launchScene);
         setupKeyHandlers(primaryStage);
         
-        currentLocation = new Location();
+        MistsGame = new Game();
         
         primaryStage.show();
         running = true;
@@ -82,39 +70,12 @@ public class Mists extends Application implements Global {
             
                 double elapsedNanoTime = (currentNanoTime - previousNanoTime) / 1000000000.0;
                 previousNanoTime = currentNanoTime;
-                tick(elapsedNanoTime);
-                render(locationCanvas);
-                
+                MistsGame.tick(elapsedNanoTime, inputLog);
+                MistsGame.render(locationCanvas);          
             } 
          }.start();
-        
-        
     } 
-    
-    
-    public void tick(double time) {
-        /*
-        * Tick checks keybuffer, initiates actions and does just about everything.
-        * Tick needs to know how much time has passed since the last tick, so it can
-        * even out actions and avoid rollercoaster game speed. 
-        */
-        
-        handleKeyPress(inputLog);
-        currentLocation.update(time);
-
-    }
-    
-    public void render(Canvas canvas) {
-        /*
-        * Render handles updating the game window, and should be called every time something needs refreshed.
-        * By default render is called 60 times per second (or as close to as possible) by AnimationTimer -thread.
-        */
-        GraphicsContext gc = canvas.getGraphicsContext2D();
-        gc.clearRect(0, 0, 800, 600);
-        currentLocation.render(gc);
-        //logger.info("Rendered on canvas");
-    }
-
+ 
     
     public static void main (String[] args) {
         logger.info("Mists game launching...");
@@ -122,39 +83,6 @@ public class Mists extends Application implements Global {
         logger.info("Mists game ended");
     }
     
-    private void handleKeyPress(ArrayList<String> inputLog) {
-        /*
-        * TODO: External loadable configfile for keybindings
-        * (probably via a class of its own)
-        */
-        
-        currentLocation.getPlayer().stopMovement();
-        
-        if (inputLog.isEmpty()) {
-            return;
-        }
-
-        
-        if (inputLog.contains("UP")) {
-            logger.log(Level.INFO, "Moving {0} UP", currentLocation.getPlayer().getName());
-            currentLocation.getPlayer().moveTowards(Direction.UP);            
-        }
-        if (inputLog.contains("DOWN")) {
-            logger.log(Level.INFO, "Moving {0} DOWN", currentLocation.getPlayer().getName());
-            currentLocation.getPlayer().moveTowards(Direction.DOWN);
-        }
-        if (inputLog.contains("LEFT")) {
-            logger.log(Level.INFO, "Moving {0} LEFT", currentLocation.getPlayer().getName());
-            currentLocation.getPlayer().moveTowards(Direction.LEFT);
-        }
-        if (inputLog.contains("RIGHT")) {
-            logger.log(Level.INFO, "Moving {0} RIGHT", currentLocation.getPlayer().getName());
-            currentLocation.getPlayer().moveTowards(Direction.RIGHT);
-        }
-        
-        
-        
-    }
     
     private void setupKeyHandlers(Stage primaryStage) {
         /** KeyPresses and releases are stored separately, so that holding down a button continues to execute commands **/
