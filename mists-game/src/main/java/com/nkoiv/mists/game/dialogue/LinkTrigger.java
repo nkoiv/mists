e4289db1c27863280adffbfccc7d575df6144b3a
nkoiv <niko.koivumaki@gmail.com>
2016-03-02 18:19:27 +0200

    Building triggers for dialogue

---
+++ mists-game/src/main/java/com/nkoiv/mists/game/dialogue/LinkTrigger.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.nkoiv.mists.game.dialogue;
+
+/**
+ *
+ * @author nikok
+ */
+public class LinkTrigger {
+    
+}
