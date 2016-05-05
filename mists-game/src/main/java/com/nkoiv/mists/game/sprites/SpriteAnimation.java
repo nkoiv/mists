671916d1aa531ca192fe68e7ea318489241b8794
nkoiv <niko.koivumaki@gmail.com>
2016-05-05 07:49:16 +0300

    Cleaned SpriteAnimation code some to be more readable

--- mists-game/src/main/java/com/nkoiv/mists/game/sprites/SpriteAnimation.java
+++ mists-game/src/main/java/com/nkoiv/mists/game/sprites/SpriteAnimation.java
@@ -21,14 +21,14 @@ import javafx.scene.paint.Color;
 
 public class SpriteAnimation {
 
-    private final ImageView imageView;
-    private final int frameCount;
-    private final int offsetX;
-    private final int offsetY;
-    private final int startX;
-    private final int startY;
-    private final double frameWidth;
-    private final double frameHeight;
+    private ImageView imageView;
+    private int frameCount;
+    private int offsetX;
+    private int offsetY;
+    private int startX;
+    private int startY;
+    private double frameWidth;
+    private double frameHeight;
     private int frameDurationMs;
 
     private int currentFrame;
@@ -38,17 +38,27 @@ public class SpriteAnimation {
     public SpriteAnimation(ImageView imageView, int frameCount, int startX, int startY, int offsetX, int offsetY, int frameWidth, int frameHeight) {
         this.imageView = imageView;
         this.frameCount = frameCount;
-        this.offsetX   = offsetX;
-        this.offsetY   = offsetY;
-        this.frameWidth     = frameWidth;
-        this.frameHeight    = frameHeight;
+        this.offsetX = offsetX;
+        this.offsetY = offsetY;
+        this.frameWidth = frameWidth;
+        this.frameHeight = frameHeight;
         this.frameDurationMs = 500;
         this.currentFrame = 0;
         this.lastFrameChangeMs = 0;
         this.startX = startX;
         this.startY = startY;
         this.snapshots = new Image[frameCount];
-        
+    }
+    
+    public SpriteAnimation(Image[] frameImages) {
+        this.imageView = null;
+        this.frameCount = frameImages.length;
+        this.frameWidth = frameImages[0].getWidth();
+        this.frameHeight = frameImages[1].getHeight();
+        this.frameDurationMs = 500;
+        this.currentFrame = 0;
+        this.lastFrameChangeMs = 0;
+        this.snapshots = frameImages;
     }
     
     
@@ -67,11 +77,6 @@ public class SpriteAnimation {
     public Image getCurrentFrame () {
         if (this.lastFrameChangeMs == 0) {
             this.lastFrameChangeMs = System.currentTimeMillis();
-            imageView.setViewport(new Rectangle2D((currentFrame * frameWidth)+(currentFrame* offsetX)+startX, 0+startY, frameWidth, frameHeight));
-            WritableImage snapshot = null;
-            SnapshotParameters parameters = new SnapshotParameters();
-            parameters.setFill(Color.TRANSPARENT);
-            return this.snapshots[currentFrame] = imageView.snapshot(parameters, snapshot);
         }
         if ((System.currentTimeMillis() - lastFrameChangeMs) > this.frameDurationMs) {
             if (this.currentFrame < this.frameCount-1) {
@@ -81,28 +86,31 @@ public class SpriteAnimation {
             }
             this.lastFrameChangeMs = System.currentTimeMillis();
             //Mists.logger.info("Changed to next frame");
-            if (this.snapshots[currentFrame] != null) return this.snapshots[currentFrame];
-            imageView.setViewport(new Rectangle2D((currentFrame * frameWidth)+(currentFrame* offsetX)+startX, 0+startY, frameWidth, frameHeight));
-            WritableImage snapshot = null;
-            SnapshotParameters parameters = new SnapshotParameters();
-            parameters.setFill(Color.TRANSPARENT);
-            this.snapshots[currentFrame] = imageView.snapshot(parameters, snapshot);
-            return this.snapshots[currentFrame];
+        }
+        if (this.snapshots[currentFrame] == null) {
+            this.snapshots[currentFrame] = snapshotFrame(currentFrame);
         }
         return this.snapshots[currentFrame];
     }
     
-    
-    // TODO: Rip images from ImageView
-    // Currently unusable
-    public ArrayList<Image> getImages() {
-       ArrayList<Image> imageList = new ArrayList<>();
-       for (int i=0; i<frameCount;i++) {
-           //imageList.add(new Image(imageView.getImage().));
-       }
-        
-       return imageList;
+    private void buildFramesFromImageview() {
+        for (int i = 0; i < this.frameCount; i++) {
+            this.snapshots[i] = snapshotFrame(i);
+        }
     }
-
+    
+    private Image snapshotFrame(int frameNumber) {
+        if (this.imageView == null) return new WritableImage((int)frameWidth, (int)frameHeight);
+        Rectangle2D r = new Rectangle2D((frameNumber * frameWidth)+(frameNumber* offsetX)+startX, 0+startY, frameWidth, frameHeight);
+        imageView.setViewport(r);
+        WritableImage snapshot = null;
+        SnapshotParameters parameters = new SnapshotParameters();
+        parameters.setFill(Color.TRANSPARENT);
+        return imageView.snapshot(parameters, snapshot);
+    }
+    
+    public Image[] getFrames() {
+        return this.snapshots;
+    }
 
 }
