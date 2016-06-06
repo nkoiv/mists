239b7be1206ef685ea63e57600de873b26780ae5
nkoiv <niko.koivumaki@gmail.com>
2016-06-06 15:22:00 +0300

    Updated template-description

--- mists-game/src/main/java/com/nkoiv/mists/game/gameobject/Templatable.java
+++ mists-game/src/main/java/com/nkoiv/mists/game/gameobject/Templatable.java
@@ -8,6 +8,9 @@ package com.nkoiv.mists.game.gameobject;
 /**
  * Templatable MabObjects can be used as
  * templates to create more of the same kind
+ * This is in some way the opposite of serializing,
+ * as templates are meant to discard unnecessary
+ * data and states
  * @author nikok
  */
 public interface Templatable {
